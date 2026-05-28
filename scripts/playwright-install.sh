808a00a0cb40eeaef4a3abec033ae28374abe494
rlcarmichael1 <150048273+rlcarmichael1@users.noreply.github.com>
2026-05-28 15:03:39 +0000

    update playwright config and scripts to install mcp and properly install playwright.

--- scripts/playwright-install.sh
+++ scripts/playwright-install.sh
@@ -5,10 +5,12 @@ cd /workspaces/workflowsascode
 
 npm install -D @playwright/test
 
-#npm install @playwright/cli --yes
+#npm install @playwright/cli --yes  -  THIS SHOULD BE INSTALLED IN THE devcontainer extensions
 
 npx --yes playwright install --with-deps chromium 
 
-#npx --yes @playwright/mcp@latest 
+#npx --yes @playwright/mcp@latest - DON'T DO THIS HERE.
 
 npx --yes playwright-cli install --skills
+
+npm install -D @playwright/mcp
