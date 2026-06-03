a8ed125654664bb4367891b982cb3b310ecde098
rlcarmichael1 <150048273+rlcarmichael1@users.noreply.github.com>
2026-06-03 21:30:27 +0000

    Login and use AI

--- tests/test-8.spec.ts
+++ tests/test-8.spec.ts
@@ -2,12 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://nexuscoreai.azurewebsites.net/LoginPage?ReturnUrl=%2F');
-  await page.getByRole('textbox', { name: 'User Name' }).click();
   await page.getByRole('textbox', { name: 'User Name' }).fill('Amy Jackson');
-  await page.getByRole('textbox', { name: 'User Name' }).press('Tab');
+  //await page.getByRole('textbox', { name: 'User Name' }).fill('Amy Jackson');
+  //await page.getByRole('textbox', { name: 'User Name' }).press('Tab');
   await page.getByRole('textbox', { name: 'Password' }).fill('AmyJackson');
-  await page.getByRole('textbox', { name: 'Password' }).press('Enter');
-  await page.getByRole('textbox', { name: 'Describe your issue, request' }).click();
+  await page.getByRole('button', { name: 'Log In' }).click();
   await page.getByRole('textbox', { name: 'Describe your issue, request' }).fill('printer queue is full');
   await page.getByRole('textbox', { name: 'Describe your issue, request' }).press('Enter');
   await page.locator('div').filter({ hasText: 'Northstar AI SupportActive' }).nth(3).click();
