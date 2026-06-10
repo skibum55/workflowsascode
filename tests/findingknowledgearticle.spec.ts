8d58bc6f5b760cb63f21070a56081e5b6c400cfc
rlcarmichael1 <150048273+rlcarmichael1@users.noreply.github.com>
2026-06-10 19:09:28 +0000

    Adding test file named findingknowledgearticle

---
+++ tests/findingknowledgearticle.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('finding a knowledge article', async ({ page }) => {
+  await page.goto('https://nexuscoreai.azurewebsites.net');
+  await page.getByRole('textbox', { name: 'User Name' }).fill('Amy Jackson');
+  await page.getByRole('textbox', { name: 'Password' }).fill('AmyJackson');
+  await page.getByRole('button', { name: 'Log In' }).press('Enter');
+  await page.getByRole('textbox', { name: 'Describe what you need help' }).fill('printer queue full');
+  await page.getByRole('button', { name: 'Ask Verto' }).press('Enter');
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Print Job Fails or Gets Stuck' }).click();
+  const page1 = await page1Promise;
+});
